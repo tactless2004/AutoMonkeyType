cb056669acb62716419953cc57358106cab07885
tactless2004 <leyton.mckinney@gmail.com>
2025-03-07 18:00:02 -0600

    Changed driver.py name, added arg parsing

--- automt.py
+++ automt.py
@@ -1,4 +1,5 @@
 from time import sleep, time_ns
+import argparse
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
@@ -7,25 +8,35 @@ from selenium.webdriver.common.action_chains import ActionChains # Sends generic
 
 def get_words(driver: webdriver) -> iter:
     words = driver.find_elements(By.CSS_SELECTOR, "div.word")
+    words_list = word.text for word in words
+    print(words_list)
     return iter(words)
 
-def test_over(driver: webdriver) -> bool:
-    words = driver.find_elements(By.CSS_SELECTOR, "div.word")
-    if words == []:
-        return True
-    else: 
-        return False
-
 if __name__ == "__main__":
-    service = Service(executable_path="chromedriver.exe")
-    driver = webdriver.Chrome(service=service)  # Optional argument, if not specified will search path.
+    # Argument Parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--wpm", help="The intended wpm you want to Auto-MonkeyType at.")
+    args = parser.parse_args()
+    if args.wpm:
+        try:
+            wpm = int(args.wpm)
+        except ValueError:
+            print("Please provide a valid wpm.")
+            exit()
+    else:
+        wpm = 200
+    delay = 60/wpm # This will never yield the intended wpm exactly, but its close.
 
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
     driver.get("https://monkeytype.com/")
 
-    sleep(1)
+    # Let the page load
+    sleep(0.5)
 
-    # Accept Cookies
+    # Try Accept Cookies
     button = driver.find_element(By.CLASS_NAME, "acceptAll")
+
     if button:
         button.click()
 
@@ -36,30 +47,28 @@ if __name__ == "__main__":
     initial_time = time_ns()
     running = True
     word_iterator = get_words(driver)
-    action = ActionChains(driver)
-    wpm = 100
-    fudge = 20
-    delay = (60/(wpm-fudge))
-    
+    action = ActionChains(driver)    
+    stop_time = 30 
+
     while running:
         try:
-            action.send_keys(next(word_iterator).text)
+            word = next(word_iterator).text
+            action.send_keys(word)
             action.send_keys(" ")
             action.perform()
+            print(f"SENT: {word}")
         except StopIteration:
             word_iterator = get_words(driver)
+            continue
         except Exception as e:
             # Generic Exception, usually the typing page is dead 
             print(f"{e}")
             running = False
 
-        # print(f"Remaining Time: {(time_ns()-initial_time)*1e-9}s")
-
-        if (time_ns() - initial_time)*1e-9 > 29.5:
+        if (time_ns() - initial_time)*1e-9 > stop_time:
             running = False
-        if test_over(driver):
-            print("test_over :(")
 
         sleep(delay)
-    sleep(20)
+
+    sleep(3)
     driver.close()
